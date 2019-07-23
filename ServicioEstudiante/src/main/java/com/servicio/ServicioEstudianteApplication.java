b5a2b0ca81559c5fff9c0c23ebf6daf924e46f82
garychavez <52689437+garychavez@users.noreply.github.com>
2019-07-23 10:56:14 -0500

    .

---
+++ ServicioEstudiante/src/main/java/com/servicio/ServicioEstudianteApplication.java
@@ -0,0 +1,13 @@
+package com.servicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioEstudianteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioEstudianteApplication.class, args);
+	}
+
+}
